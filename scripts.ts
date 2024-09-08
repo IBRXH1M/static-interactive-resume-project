e3587d6a9dc3599d3ddabae5fb12b2b416833475
Muhammad Ibrahim Khan <149921040+IBRXH1M@users.noreply.github.com>
2024-09-08 15:26:12 +0500

    Add files via upload

---
+++ scripts.ts
@@ -0,0 +1,12 @@
+const toggleButton = document.getElementById('toggleSkills');
+const skillsSection = document.getElementById('skills');
+
+if (toggleButton && skillsSection) {
+  toggleButton.addEventListener('click', () => {
+    if (skillsSection.style.display === 'none') {
+      skillsSection.style.display = 'block';
+    } else {
+      skillsSection.style.display = 'none';
+    }
+  });
+}
